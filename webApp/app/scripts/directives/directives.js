8f676ab8c3b1f65a67a880c7a52ff2c4290f3cfd
zhangxishuo <zhangxishuo1998@outlook.com>
2019-12-14 18:09:44 +0800

    修改配置

--- webApp/app/scripts/directives/directives.js
+++ webApp/app/scripts/directives/directives.js
@@ -325,10 +325,6 @@ function landingScrollspy(){
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
-            element.scrollspy({
-                target: '.navbar-fixed-top',
-                offset: 80
-            });
         }
     }
 }
